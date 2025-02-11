3cb0f2b25f0521bb431d8c90f9c079c16eb485cc
Meet-J-Shah <tatva_pcq160@hotmail.com>
2025-02-11 17:55:41 +0530

    test cases

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,15 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' },rules: {
-    
-    "no-unused-vars":"warn",
-    "prefer-const": ["error", { "ignoreReadBeforeAssign": true }]
-} },
+  {
+    ignores: ['.tests/'],
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+    rules: {
+      'no-unused-vars': 'warn',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  
 ];
